69431cabd20bb2f733abdff32ca5426b790b5ba8
anshubeta <tiwri35@gmail.com>
2026-01-21 05:25:41 +0530

    Feature: Implement get all problems and problem by id

--- src/podVerdict/services/problems/get-problems.controller.ts
+++ src/podVerdict/services/problems/get-problems.controller.ts
@@ -20,3 +20,39 @@ export const getAllProblems = async (req: Request, res: Response) => {
         res.status(500).json({ error: "Failed to fetch problems" });
     }
 };
+
+export const getProblemById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        if (!id || typeof id !== "string") {
+            return res.status(400).json({ error: "Problem ID is required" });
+        }
+
+        const problem = await prisma.problem.findUnique({
+            where: { id },
+            include: {
+                author: {
+                    select: {
+                        id: true,
+                        username: true,
+                        email: true
+                    }
+                },
+                testCases: true
+            }
+        });
+
+        if (!problem) {
+            return res.status(404).json({ error: "Problem not found" });
+        }
+
+        res.status(200).json({
+            message: "Problem retrieved successfully",
+            problem
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
--- src/podVerdict/services/routes/problem.routes.ts
+++ src/podVerdict/services/routes/problem.routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { verifyToken, authorizeRole } from "../middlewares/authMiddleware";
 import { createProblem } from "../problems/create-problem.controller";
 import { Role } from "../../../generated/prisma";
-import { getAllProblems } from "../problems/get-problems.controller";
+import { getAllProblems, getProblemById } from "../problems/get-problems.controller";
 
 const problemRouter = Router();
 
 problemRouter.get("/", getAllProblems)
+problemRouter.get("/:id", getProblemById)
 
 problemRouter.post("/", verifyToken, authorizeRole(Role.ADMIN), createProblem);
 
